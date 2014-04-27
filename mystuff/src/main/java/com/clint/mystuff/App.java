49b41c23d123c8480d7cef7e746f591e70d76d0a
Clint Willard <clint317@gmail.com>
2014-04-27 17:25:56 -0400

    We did work

--- mystuff/src/main/java/com/clint/mystuff/App.java
+++ mystuff/src/main/java/com/clint/mystuff/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main( String[] args ) {
     	
     	SaySomething saySomething = new SaySomething();
-        System.out.println(saySomething.something("Yowsa! That's not right!"));
+        System.out.println(saySomething.something("Yowsa! We doing work!"));
         
     }
     
