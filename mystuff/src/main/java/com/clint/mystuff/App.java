3b4f2e9ff027c49eb967c2394d032e624e5f87b1
Clint Willard <clint317@gmail.com>
2014-04-27 13:05:25 -0400

    Added say something.

--- mystuff/src/main/java/com/clint/mystuff/App.java
+++ mystuff/src/main/java/com/clint/mystuff/App.java
@@ -1,13 +1,12 @@
 package com.clint.mystuff;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "I am legend!" );
+public class App {
+	
+    public static void main( String[] args ) {
+    	
+    	SaySomething saySomething = new SaySomething();
+        System.out.println(saySomething.something());
+        
     }
+    
 }
